add4476687713fe8f9cc65d714865dfd99ebf6ae
Henrique <hacmelo@gmail.com>
2020-11-13 14:16:28 -0300

    Finalizando o front da página de login

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:agile_gas_app/screens/services/auth.dart';
 import 'package:agile_gas_app/shared/constants.dart';
 import 'package:agile_gas_app/shared/loading.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget{
@@ -30,34 +31,96 @@ class _SignInState extends State<SignIn>{
       backgroundColor: Colors.black,
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        decoration: BoxDecoration(
-            image: new DecorationImage(
-                image: new AssetImage("images/logo.png"), fit: BoxFit.contain),
-        ),
+
         child: Form(
           key: _formKey,
           child: Column(
             children: <Widget>[
-              SizedBox(height: 400.0),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Email: exemplo@email.com'),
-                validator: (val) => val.isEmpty ? 'Insira um e-mail.' : null, //verifica se o campo está vazio
-                onChanged: (val){ //toda vez que o valor do campo mudar
-                  setState(() => email = val); //mude o valor da variável email para o valor do campo
-                }
+              SizedBox(height: 160.0),
+              Image.asset('images/logo.png'),
+              SizedBox(height: 130),
+              Container(
+                width: 250,
+                height: 17,
+                child:
+                TextFormField(
+                    cursorColor: Colors.white,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: textInputDecoration.copyWith(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        hintText: 'exemplo@email.com',
+                        hintStyle: TextStyle(fontSize: 15.0, color: Colors.white),
+                        prefixIcon: PictureWidget(),
+                        prefixText: '  ',
+                        prefixIconConstraints: BoxConstraints(
+                          minHeight: 25,
+                          minWidth: 25,
+                        ),
+                        filled: true,
+                        fillColor: Colors.black,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        )
+                    ),
+
+                    validator: (val) => val.isEmpty ? 'Insira um e-mail.' : null, //verifica se o campo está vazio
+                    onChanged: (val){ //toda vez que o valor do campo mudar
+                      setState(() => email = val); //mude o valor da variável email para o valor do campo
+                    }
+                ),
+
               ),
+              Image.asset('images/line.png', color: Colors.white, width: 200),
+
               SizedBox(height: 20.0),
-              TextFormField(
-                  decoration: textInputDecoration.copyWith(hintText: 'Senha'),
-                  obscureText: true,
-                  validator: (val) => val.length < 6 ? 'Senha incorreta!' : null, //verifica se a senha é menor que 6 caracteres
-                  onChanged: (val){ //toda vez que o valor do campo mudar
-                    setState(() => password = val); //mude o valor da variaǘel senha para o valor do campo
-                  }
+
+              Container(
+                width: 250,
+                height: 17,
+                child:
+                TextFormField(
+                    cursorColor: Colors.white,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: textInputDecoration.copyWith(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        hintText: 'Senha',
+                        hintStyle: TextStyle(fontSize: 15.0, color: Colors.white),
+                        labelStyle: TextStyle(color: Colors.red),
+                        prefixIcon: PictureWidget2(),
+                        prefixText: '  ',
+
+                        prefixIconConstraints: BoxConstraints(
+                          minHeight: 25,
+                          minWidth: 25,
+                        ),
+                        filled: true,
+                        fillColor: Colors.black,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        )
+                    ),
+                    obscureText: true,
+                    validator: (val) => val.length < 6 ? 'Senha incorreta!' : null, //verifica se a senha é menor que 6 caracteres
+                    onChanged: (val){ //toda vez que o valor do campo mudar
+                      setState(() => password = val); //mude o valor da variaǘel senha para o valor do campo
+                    }
+                ),
               ),
-              SizedBox(height: 20.0),
+              Image.asset('images/line.png', color: Colors.white, width: 200),
+
+              SizedBox(height: 40.0),
               RaisedButton(
-                color: Colors.white,
+              color: Colors.white,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              padding: EdgeInsets.symmetric(horizontal: 120, vertical: 15),
                 child: Text(
                   'LOGIN',
                   style: TextStyle(color: Colors.black)
@@ -75,8 +138,11 @@ class _SignInState extends State<SignIn>{
                   }
                 }
               ),
+              SizedBox(height: 30),
               RaisedButton(
-                  color: Colors.black,
+                  color: Colors.grey[850],
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+
                   child: Text(
                       'Create your account!',
                       style: TextStyle(color: Colors.white)
@@ -86,7 +152,8 @@ class _SignInState extends State<SignIn>{
                   }
               ),
               RaisedButton(
-                  color: Colors.black,
+                  color: Colors.grey[850],
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
                   child: Text(
                       'Forgot you password?',
                       style: TextStyle(color: Colors.white)
@@ -95,7 +162,6 @@ class _SignInState extends State<SignIn>{
 
                   }
               ),
-              SizedBox(height: 12.0),
               Text(
                 error,
                 style: TextStyle(color: Colors.red, fontSize: 14.0),
@@ -105,5 +171,22 @@ class _SignInState extends State<SignIn>{
         ),
       ),
     );
+  }
+}
+
+class PictureWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new ImageIcon (new AssetImage('images/username.jpg'),
+    color: Colors.white);
+  }
+}
+
+
+class PictureWidget2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new ImageIcon (new AssetImage('images/password.png'),
+        color: Colors.white);
   }
 }
