42b27f854798e7d1ac41bf56f47e1851148f03e8
Eduarda Servidio <dudaservidio@gmail.com>
2020-11-14 01:33:56 +0000

    Algumas mudanças visuais na tela de registro

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -26,9 +26,8 @@ class _RegisterState extends State<Register>{
   @override
   Widget build(BuildContext context){
     return loading ? Loading() : Scaffold(
-      backgroundColor: Colors.brown[100],
       appBar: AppBar(
-        backgroundColor: Colors.brown[400],
+        backgroundColor: Colors.black,
         elevation: 0.0,
         title: Text('Sign Up to Agile Gas'),
         actions: <Widget>[
@@ -41,35 +40,168 @@ class _RegisterState extends State<Register>{
           )
         ],
       ),
-      body: Container(
+      body: Container(decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/agile_gas_back.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+
         child: Form(
           key: _formKey,
           child: Column(
-              children: <Widget>[
-                SizedBox(height: 20.0),
+            children: <Widget>[
+              SizedBox(height: 80.0),
+              Image.asset('images/logo.png'),
+              SizedBox(height: 10),
+              RaisedButton(
+                  color: Colors.transparent,
+                  child: Text(
+                      'Create your account!',
+                      style: TextStyle(color: Colors.white, fontSize: 20)
+                  ),
+              ),
+              SizedBox(height: 30.0),
+              Container(
+                width: 250,
+                height: 17,
+                child:
                 TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Email'),
-                    validator: (val) => val.isEmpty ? 'Insira um e-mail' : null, //verifica se o campo está vazio
+                    cursorColor: Colors.white,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: textInputDecoration.copyWith(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        hintText: 'Insira um email',
+
+                        hintStyle: TextStyle(fontSize: 15.0, color: Colors.white.withOpacity(0.6)),
+                        prefixIcon: PictureWidget(),
+                        prefixText: '  ',
+                        prefixIconConstraints: BoxConstraints(
+                          minHeight: 25,
+                          minWidth: 25,
+                        ),
+                        filled: true,
+                        fillColor: Colors.black,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        )
+                    ),
+
+                    validator: (val) => val.isEmpty ? 'Insira um e-mail correto.' : null, //verifica se o campo está vazio
                     onChanged: (val){ //toda vez que o valor do campo mudar
-                      setState(() => email = val); ////mude o valor da variável email para o valor do campo
+                      setState(() => email = val); //mude o valor da variável email para o valor do campo
                     }
                 ),
-                SizedBox(height: 20.0),
+
+              ),
+              Image.asset('images/line.png', color: Colors.white, width: 200),
+
+              SizedBox(height: 20.0),
+
+              Container(
+                width: 250,
+                height: 17,
+                child:
                 TextFormField(
-                    decoration: textInputDecoration.copyWith(hintText: 'Senha'),
+                    cursorColor: Colors.white,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: textInputDecoration.copyWith(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        hintText: 'Insira uma Senha',
+                        hintStyle: TextStyle(fontSize: 15.0, color: Colors.white.withOpacity(0.6)),
+                        labelStyle: TextStyle(color: Colors.red),
+                        prefixIcon: PictureWidget2(),
+                        prefixText: '  ',
+
+                        prefixIconConstraints: BoxConstraints(
+                          minHeight: 25,
+                          minWidth: 25,
+                        ),
+                        filled: true,
+                        fillColor: Colors.black,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        )
+                    ),
+                    obscureText: true,
                     validator: (val) => val.length < 6 ? 'Insira uma senha com +6 caracteres' : null, //verifica se a senha é menor que 6 caracteres
-                    obscureText: true, //esconde caracteres escritos
                     onChanged: (val){ //toda vez que o valor do campo mudar
                       setState(() => password = val); //mude o valor da variaǘel senha para o valor do campo
                     }
                 ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                    color: Colors.pink[400],
+              ),
+              Image.asset('images/line.png', color: Colors.white, width: 200),
+
+              SizedBox(height: 20.0),
+              Container(
+                width: 250,
+                height: 17,
+                child:
+                TextFormField(
+                    cursorColor: Colors.white,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: textInputDecoration.copyWith(
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 20.0),
+                        hintText: 'Confirme a Senha',
+                        hintStyle: TextStyle(fontSize: 15.0, color: Colors.white.withOpacity(0.6)),
+                        labelStyle: TextStyle(color: Colors.red),
+                        prefixIcon: PictureWidget2(),
+                        prefixText: '  ',
+
+                        prefixIconConstraints: BoxConstraints(
+                          minHeight: 25,
+                          minWidth: 25,
+                        ),
+                        filled: true,
+                        fillColor: Colors.black,
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            borderSide: BorderSide(color: Colors.transparent)
+                        )
+                    ),
+                    obscureText: true,
+                    validator: (val) => val.length < 6 ? 'Insira uma senha com +6 caracteres' : null, //verifica se a senha é menor que 6 caracteres
+                    onChanged: (val){ //toda vez que o valor do campo mudar
+                      setState(() => password = val); //mude o valor da variaǘel senha para o valor do campo
+                    }
+                ),
+              ),
+
+              Image.asset('images/line.png', color: Colors.white, width: 200),
+
+              SizedBox(height: 40.0),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.lightGreen.withOpacity(0.5),
+                      spreadRadius: 3,
+                      blurRadius: 7,
+                      offset: Offset(1, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                child: RaisedButton(
+                    color: Colors.white,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+                    padding: EdgeInsets.symmetric(horizontal: 120, vertical: 15),
+
                     child: Text(
-                        'Register',
-                        style: TextStyle(color: Colors.white)
+                        'REGISTER',
+                        style: TextStyle(color: Colors.black)
                     ),
                     onPressed: () async {
                       if (_formKey.currentState.validate()) { //verifica se esse formulário possui tipos de dados corretos
@@ -83,16 +215,30 @@ class _RegisterState extends State<Register>{
                         }
                       }
                     }
+
                 ),
-                SizedBox(height: 12.0),
-                Text(
-                  error,
-                  style: TextStyle(color: Colors.red, fontSize: 14.0),
-                ),
-              ]
+              ),
+            ]
           ),
         ),
       ),
     );
+  }
+}
+
+class PictureWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new ImageIcon (new AssetImage('images/username.jpg'),
+    color: Colors.white);
+  }
+}
+
+
+class PictureWidget2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new ImageIcon (new AssetImage('images/password.png'),
+        color: Colors.white);
   }
 }
