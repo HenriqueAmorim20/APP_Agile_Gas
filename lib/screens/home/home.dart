a33d82cdb493bf0fad8cc10a73e4a237d1489dd3
Henrique <hacmelo@gmail.com>
2020-11-18 02:52:37 -0300

    Terminando o visual do drawer da home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -5,71 +5,146 @@ class Home extends StatelessWidget {
 
   final AuthService _auth = AuthService();
 
+  String nome='Henrique';
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      backgroundColor: Colors.grey [900],
+      backgroundColor: Colors.black,
       appBar: new AppBar(
         backgroundColor: Colors.grey[900],
         title: Image.asset('images/logo.png', color: Colors.red, height: 50, width: 150,),
       ),
       body: Center(child: Text('Teste')),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-                child: Text('Cabeçalho'),
-                decoration: BoxDecoration(
-                  color: Colors.red,
+      drawer: ClipRRect(
+        borderRadius: BorderRadius.only(
+             bottomRight: Radius.circular(45)),
+        child: Drawer(
+          child: Container(
+            color: Colors.grey[900],
+            child: ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                _createHeader(),
+                Container(
+                  color: Colors.grey[900],
+                  child: new Column(
+                      children: [
+                        ListTile(
+                            leading: Icon(Icons.directions_car_rounded, color: Colors.white),
+                            title: Align(
+                              child: Text('Veículos', style: TextStyle(color: Colors.white, fontSize:15 )),
+                              alignment: Alignment(-1.35,0),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                            }
+                        ),
+                        ListTile(
+                            leading: Icon(Icons.attach_money_outlined, color: Colors.white),
+                            title: Align(
+                              child: Text('Acompanhar gastos', style: TextStyle(color: Colors.white, fontSize:15 )),
+                              alignment: Alignment(-1.8,0),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                            }
+                        ),
+                        Divider(color: Colors.white.withOpacity(0.5),),
+
+                        ListTile(
+                            leading: Icon(Icons.water_damage, color: Colors.white),
+                            title: Align(
+                              child: Text('Cadastrar posto', style: TextStyle(color: Colors.white, fontSize:15 )),
+                              alignment: Alignment(-1.5,0),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                            }
+                        ),
+                        ListTile(
+                            leading: Icon(Icons.local_gas_station, color: Colors.white),
+                            title: Align(
+                              child: Text('Registrar abastecimento', style: TextStyle(color: Colors.white, fontSize:15 )),
+                              alignment: Alignment(-2.1,0),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                            }
+                        ),
+                        Divider(color: Colors.white.withOpacity(0.5),),
+            
+                        ListTile(
+                            leading: Icon(Icons.article_outlined, color: Colors.white),
+                            title: Align(
+                              child: Text('Deixe sua sugestão!', style: TextStyle(color: Colors.white, fontSize:15 )),
+                              alignment: Alignment(-1.7,0),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                            }
+                        ),
+                        ListTile(
+                            leading: Icon(Icons.settings, color: Colors.white),
+                            title: Align(
+                              child: Text('Configurações', style: TextStyle(color: Colors.white, fontSize:15 )),
+                              alignment: Alignment(-1.5,0),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                            }
+                        ),
+                        Divider(color: Colors.white.withOpacity(0.5),),
+                        SizedBox(height: 15,),
+                        ListTile(
+                            leading: Icon(Icons.logout, color: Colors.white),
+                            title: Align(
+                              child: Text('Sair', style: TextStyle(color: Colors.white, fontSize:15 )),
+                              alignment: Alignment(-1.3,0),
+                            ),
+                            onTap: () {
+                              Navigator.pop(context);
+                              _auth.signOut();
+                            }
+                        ),
+                      ]
+                  ),
                 ),
+
+              ],
             ),
-            ListTile(
-              title: Text('Veículos'),
-              onTap: () {
-                Navigator.pop(context);
-              }
-            ),
-            ListTile(
-                title: Text('Acompanhar Gastos'),
-                onTap: () {
-                  Navigator.pop(context);
-                }
-            ),
-            ListTile(
-                title: Text('Cadastrar Posto'),
-                onTap: () {
-                  Navigator.pop(context);
-                }
-            ),
-            ListTile(
-                title: Text('Registrar Abastecimento'),
-                onTap: () {
-                  Navigator.pop(context);
-                }
-            ),
-            ListTile(
-                title: Text('Configurações'),
-                onTap: () {
-                  Navigator.pop(context);
-                }
-            ),
-            ListTile(
-                title: Text('Deixe sua Sugestão'),
-                onTap: () {
-                  Navigator.pop(context);
-                }
-            ),
-            ListTile(
-                title: Text('Sair'),
-                onTap: () {
-                  Navigator.pop(context);
-                   _auth.signOut();
-                }
-            ),
-          ],
-        ),
-      ),
+          ),
+        ),),
+
     );
+  }
+  Widget _createHeader() {
+    return DrawerHeader(
+        margin: EdgeInsets.zero,
+        padding: EdgeInsets.zero,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.fill,
+                image:  AssetImage('images/posto.jpg'))),
+        child: Stack(children: <Widget>[
+          Positioned(
+              bottom: 12.0,
+              left: 16.0,
+              child: Text("Olá, $nome!",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w500))),
+        ]));
+  }
+
+}
+
+
+class PictureWidget4 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new ImageIcon (new AssetImage('images/dot.png'),
+        color: Colors.white, size: 20, );
   }
 }
