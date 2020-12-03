03374244ae32a8df27eefb3e3163043d0ced9aee
Henrique <hacmelo@gmail.com>
2020-12-03 18:22:16 -0300

    Parte do front da tela de gastos pronta

--- lib/screens/home/car_tile_gastos.dart
+++ lib/screens/home/car_tile_gastos.dart
@@ -9,7 +9,7 @@ class CarTileGastos extends StatelessWidget {
   CarTileGastos({this.car});
 
   String mostrar_ano = 'Ano: ', mostrar_cor = 'Cor: ', mostrar_placa = 'Placa: ', mostrar_motor = 'Motor: ', mostrar_marca = 'Marca: ', mostrar_modelo = 'Modelo: ';
-
+  String gastos_data, gastos_totais, gastos_parcial, gastos_pm, gastos_combus;
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +30,7 @@ class CarTileGastos extends StatelessWidget {
                 color: Colors.black,
                 child: Container(
                   child: ListTile(
-
-                    leading: Icon(Icons.directions_car_rounded,
+                    leading: Icon(Icons.attach_money_outlined,
                       size: 40,
                       color: Colors.red,
                     ),
@@ -48,7 +47,7 @@ class CarTileGastos extends StatelessWidget {
                       context: context,
                       builder: (BuildContext context){
                         return AlertDialog(
-                          title: Text("Gastos do Veículo"),
+                          title: Text("Gastos totais R\$1023,23"),
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(10)),
                           backgroundColor: Colors.grey[200],
@@ -59,15 +58,33 @@ class CarTileGastos extends StatelessWidget {
                                 child: Column(
                                   mainAxisSize: MainAxisSize.min,
                                   children: <Widget>[
-                                    Text(" "),
                                     Divider(color: Colors.black.withOpacity(0.5)),
-                                    Text(" "),
+                                    Column(
+                                        children: [
+                                          Text("Data: ", textAlign: TextAlign.left,),
+                                          Text("Total R\$256,90"),
+                                          Text(" Litro R\$4,70"),
+                                          Text(" Tipo: Gasolina"),
+                                        ],
+                                    ),
                                     Divider(color: Colors.black.withOpacity(0.5)),
-                                    Text(" "),
+                                    Column(
+                                      children: [
+                                        Text("Data: ", textAlign: TextAlign.left,),
+                                        Text("Total R\$206,90"),
+                                        Text(" Litro R\$3,70"),
+                                        Text(" Tipo: Etanol"),
+                                      ],
+                                    ),
                                     Divider(color: Colors.black.withOpacity(0.5)),
-                                    Text(" "),
-                                    Divider(color: Colors.black.withOpacity(0.5)),
-                                    Text(" "),
+                                    Column(
+                                      children: [
+                                        Text("Data: ", textAlign: TextAlign.left,),
+                                        Text("Total R\$156,90"),
+                                        Text(" Litro R\$2,70"),
+                                        Text(" Tipo: Diesel"),
+                                      ],
+                                    ),
                                     Divider(color: Colors.black.withOpacity(0.5)),
                                   ],
                                 ),
