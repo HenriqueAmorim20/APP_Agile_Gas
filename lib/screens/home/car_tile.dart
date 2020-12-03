778f66c80be8ab82b42facbe8f2841ac2757da84
Henrique <hacmelo@gmail.com>
2020-12-03 17:02:38 -0300

    Mudando o visual da lista de carros e botando as informações no pop up

--- lib/screens/home/car_tile.dart
+++ lib/screens/home/car_tile.dart
@@ -8,68 +8,106 @@ class CarTile extends StatelessWidget {
   final Car car;
   CarTile({this.car});
 
+  String mostrar_ano = 'Ano: ', mostrar_cor = 'Cor: ', mostrar_placa = 'Placa: ', mostrar_motor = 'Motor: ', mostrar_marca = 'Marca: ', mostrar_modelo = 'Modelo: ';
+
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.only(top: 8.0),
-        child: Card(
-            margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
-            child: RaisedButton(
-                          child: ListTile(
-                leading: CircleAvatar(
-                  radius: 25.0,
-                  backgroundColor: Colors.red,
+    mostrar_ano +=car.ano;
+    mostrar_cor+=car.cor;
+    mostrar_placa +=car.placa;
+    mostrar_motor+=car.motor;
+    mostrar_modelo+=car.modelo;
+    mostrar_marca+=car.marca;
+
+    return Container(
+      margin: EdgeInsets.symmetric(vertical:5, horizontal:10),
+              child:RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                    side: BorderSide(color: Colors.white, width: 0.5)
                 ),
-                title: Text(car.modelo),
-                subtitle: Text(car.cor),
+                color: Colors.black,
+                child: Container(
+                  child: ListTile(
+
+                    leading: Icon(Icons.directions_car_rounded,
+                      size: 40,
+                      color: Colors.red,
+                    ),
+                    title: Text(car.modelo, style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.white,
+
+                    )),
+                  ),
+                ),
+                onPressed:() async{
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context){
+                        return AlertDialog(
+                          title: Text("Informações do Veículo"),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          backgroundColor: Colors.grey[200],
+                          content: Stack(
+                            overflow: Overflow.visible,
+                            children: <Widget>[
+                              Form(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Text(mostrar_placa),
+                                    Divider(color: Colors.black.withOpacity(0.5)),
+                                    Text(mostrar_marca),
+                                    Divider(color: Colors.black.withOpacity(0.5)),
+                                    Text(mostrar_cor),
+                                    Divider(color: Colors.black.withOpacity(0.5)),
+                                    Text(mostrar_motor),
+                                    Divider(color: Colors.black.withOpacity(0.5)),
+                                    Text(mostrar_ano),
+                                    Divider(color: Colors.black.withOpacity(0.5)),
+                                  ],
+                                ),
+
+                              ),
+                            ],
+                          ),
+                          actions: [
+                            Container(
+                              alignment: Alignment.bottomRight,
+                              margin: EdgeInsets.symmetric(horizontal: 0),
+                              child: RaisedButton(
+                                  color: Colors.red,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+                                  child: Text("OK", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                                  onPressed: () async {
+                                    Navigator.pop(context);
+                                  }
+                              ),
+                            ),
+                            Container(
+                              alignment: Alignment.bottomLeft,
+                              margin: EdgeInsets.symmetric(horizontal: 0),
+                              child: RaisedButton(
+                                  color: Colors.red,
+                                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                                  child: Text("DELETAR VEÍCULO", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                                  onPressed: () async {
+
+                                  }
+                              ),
+                            ),
+                          ],
+                        );
+                      }
+                  );
+                },
               ),
-            onPressed:() async{
-              showAlertDialog(context);
-            }
-        )
-        )
     );
     }
-  
-showAlertDialog(BuildContext context){
-    // configura o  AlertDialog
-    AlertDialog alerta = AlertDialog(
-      title: Text("Informações do Veiculo"),
-      actions: [
-        Container(
-          alignment: Alignment.bottomRight,
-          margin: EdgeInsets.symmetric(horizontal: 0),
-          child: RaisedButton(
-            color: Colors.red,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            padding: EdgeInsets.symmetric(horizontal: 0, vertical: 15),
-            child: Text("OK", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-            onPressed: () async {
-              Navigator.pop(context);
-            }
-          ),
-        ),
-        Container(
-          alignment: Alignment.bottomLeft,
-          margin: EdgeInsets.symmetric(horizontal: 0),
-          child: RaisedButton(
-            color: Colors.red,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
-            child: Text("DELETAR VEÍCULO", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-            onPressed: () async {
-              
-            }
-          ),
-        ),
-      ],
-    );
-    // exibe o dialog
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return alerta;
-      },
-    );
-  }
 }
